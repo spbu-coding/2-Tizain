61f09c48549a94f2b1479952e0f41d945c9d414c
Timofey Zaynulin <dark-lord-6@mail.ru>
2020-12-03 00:00:32 +0300

    Fixed some mistakes

--- src/sort.h
+++ src/sort.h
@@ -1,15 +1,9 @@
-void bubble_sort(long int array[] , int* nsort)
-{
-    for(int i = 0 ; i < *(nsort)-1 ; ++i)
-    {
-        for(int j = 0 ; j < *(nsort) - i - 1 ; ++j)
-        {
-            if(array[j] > array[j+1])
-            {
-               int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        }
-    }
-}
+#ifndef SORT_H
+#define SORT_H
+
+typedef long int* strings_array_t;
+typedef size_t array_size_t;
+
+void bubble_sort(strings_array_t, array_size_t);
+
+#endif
