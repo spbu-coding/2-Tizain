448c6d91ba1af3259c1d153b47f5310d9ee7a93c
Timofey Zaynulin <dark-lord-6@mail.ru>
2021-02-20 15:13:53 +0300

    Fix mistakes

--- src/sort.s
+++ src/sort.s
@@ -1,62 +1,43 @@
-	.file	"sort.c"
+.file	"sort.c"
 	.text
 	.p2align 4
 	.globl	sort
 	.type	sort, @function
 sort:
-.LFB23:
+.LFB0:
 	.cfi_startproc
 	endbr64
-	subq	$1, %rsi
-	je	.L17
-	pushq	%rbx
-	.cfi_def_cfa_offset 16
-	.cfi_offset 3, -16
-	xorl	%eax, %eax
-	xorl	%ebx, %ebx
-	movq	%rsi, %r11
+	cmpl	$1, %esi
+	jle	.L1
+	leal	-2(%rsi), %r8d
+	leaq	8(%rdi), %r9
 	.p2align 4,,10
 	.p2align 3
-.L2:
-	movq	%r11, %r10
-	subq	%rax, %r10
-	je	.L7
-	movq	(%rdi), %rcx
-	xorl	%edx, %edx
-	xorl	%eax, %eax
-	jmp	.L5
+.L3:
+	movl	%r8d, %edx
+	movq	%rdi, %rax
+	leaq	(%r9,%rdx,8), %rsi
 	.p2align 4,,10
 	.p2align 3
-.L18:
-	movslq	%ecx, %rcx
-	movq	%rsi, (%rdi,%r9,8)
-	movq	%rcx, (%r8)
-	cmpq	%r10, %rax
-	jnb	.L7
 .L5:
-	addl	$1, %edx
-	movq	%rax, %r9
-	movl	%edx, %eax
-	leaq	(%rdi,%rax,8), %r8
-	movq	(%r8), %rsi
-	cmpq	%rcx, %rsi
-	jl	.L18
-	movq	%rsi, %rcx
-	cmpq	%r10, %rax
-	jb	.L5
-.L7:
-	leal	1(%rbx), %eax
-	movq	%rax, %rbx
-	cmpq	%r11, %rax
-	jb	.L2
-	popq	%rbx
-	.cfi_def_cfa_offset 8
-	ret
-.L17:
-	.cfi_restore 3
+	movq	(%rax), %rdx
+	movq	8(%rax), %rcx
+	cmpq	%rcx, %rdx
+	jle	.L4
+	movslq	%edx, %rdx
+	movq	%rcx, (%rax)
+	movq	%rdx, 8(%rax)
+.L4:
+	addq	$8, %rax
+	cmpq	%rsi, %rax
+	jne	.L5
+	subl	$1, %r8d
+	cmpl	$-1, %r8d
+	jne	.L3
+.L1:
 	ret
 	.cfi_endproc
-.LFE23:
+.LFE0:
 	.size	sort, .-sort
 	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
 	.section	.note.GNU-stack,"",@progbits
